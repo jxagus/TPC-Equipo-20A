f715ab6c58314d117c5e92563b980805cc090d8b
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2026-06-03 22:37:32 -0300

    Producto y productoImagenes .cs(Dominio)

--- Dominio/Productos.cs
+++ Dominio/Productos.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    public class Producto
+    public class Productos
     {
         public int IdProducto { get; set; }
         public string NombreProducto { get; set; }
---
+++ Dominio/ProductosImagenes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ProductosImagenes
+    {
+        public int IdImagen { get; set; }
+        public int IdProducto { get; set; }
+        public string UrlImagen { get; set; }
+    }
+}
