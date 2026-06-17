3ef5a2673cd941faf532d125f8dd72c280564dd1
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2026-06-17 14:01:31 -0300

    toast (v1) para los permisos

---
+++ Resto-Bar-Web/Scripts/toast.js
@@ -0,0 +1,13 @@
+﻿function mostrarErrorYRedirigir(mensaje, url) {
+
+    const toast = document.getElementById("toastError");
+    const texto = document.getElementById("toastMensaje");
+
+    texto.innerText = mensaje;
+
+    toast.classList.add("show");
+
+    setTimeout(function () {
+        window.location.href = url;
+    }, 3000);
+}
